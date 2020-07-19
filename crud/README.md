17a26fab6efb17e15e9ea9698e88c0ac41760a67
bruno pessoa <brunopessoa.melo@gmail.com>
2020-07-19 18:21:38 -0300

    criando o componente, testando e componente renderizado.

--- crud/README.md
+++
@@ -1,1 +0,0 @@
-Projeto-CRUD-ANGULAR9
---
+++ crudangular9/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/templates/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
